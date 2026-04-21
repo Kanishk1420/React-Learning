c4942d707659866f19cdb9389dcc2cea5e3b22cd
Kanishk <Kanishkgupta2003@outlook.com>
2026-04-21 17:06:23 +0530

    Before Props Implemenation basis strucutre

--- Topic 6- Cards Project With Props Drilling/src/App.jsx
+++ Topic 6- Cards Project With Props Drilling/src/App.jsx
@@ -1,32 +1,8 @@
-import React from 'react'
-import './index.css'
-import {Bookmark} from 'lucide-react'
+import Card from "./components/Card"
 const App = () => {
   return (
     <div className='parent'>
-      <div className='card'>
-       <div>
-         <div className='top'>
-          <img src="https://pngimg.com/uploads/meta/meta_PNG12.png" alt="Meta"/>
-          <button>Save <Bookmark size={10}/></button>
-        </div>
-          <div className='center'>
-            <h3>Meta <span>5 days ago</span></h3>
-            <h2>Ai Lead Engineer</h2>
-            <div>
-              <h4>Full Time</h4>
-              <h4>Senior Level</h4>
-            </div>
-          </div>
-       </div>
-          <div className='bottom'>
-            <div>
-                <h3>70,000₹ per anum</h3>
-                <p>Remote</p>
-            </div>
-            <button>Apply Now</button>
-          </div>
-      </div>
+      <Card />
     </div>
   )
 }
