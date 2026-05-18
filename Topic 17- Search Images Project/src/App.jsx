960810dfb8c26a45f0a6faf67814e3575a37730d
Kanishk <Kanishkgupta2003@outlook.com>
2026-05-18 16:51:54 +0530

    Fetched videos,phtotos and gifs from axios

--- Topic 17- Search Images Project/src/App.jsx
+++ Topic 17- Search Images Project/src/App.jsx
@@ -1,16 +1,35 @@
 import React from "react";
-import { fetchphotos } from "./api/mediaapi";
+import { fetchgifs, fetchphotos, fetchVideos } from "./api/mediaApi";
 
 const App = () => {
   return (
     <div className="h-screen w-full bg-gray-950 text-white">
       <button
+        className="bg-green-400 px-4 py-2 m-5"
         onClick={async () => {
-          const data = await fetchphotos("office");
+          const data = await fetchphotos("Anime");
           console.log(data.results);
         }}
       >
         Get Photos
+      </button>
+      <button
+        className="bg-green-400 px-4 py-2 m-5"
+        onClick={async () => {
+          const data = await fetchVideos("Anime");
+          console.log(data.videos);
+        }}
+      >
+        Get Videos
+      </button>
+      <button
+        className="bg-green-400 px-4 py-2 m-5"
+        onClick={async () => {
+          const data = await fetchgifs("Anime");
+          console.log(data.results);
+        }}
+      >
+        Get Gifs
       </button>
     </div>
   );
