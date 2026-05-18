a7674377cc0723c31c160cacf39eec14345b7a86
Kanishk <Kanishkgupta2003@outlook.com>
2026-05-18 14:43:20 +0530

    Search Images and Videos project started

--- Topic 17- Search Images Project/src/App.jsx
+++ Topic 17- Search Images Project/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { fetchphotos } from './api/mediaapi'
 
 const App = () => {
   return (
     <div className='h-screen w-full bg-gray-950 text-white'>
-      
+      <button onClick={()=>{fetchphotos('office')}}>Get Photos</button>
     </div>
   )
 }
