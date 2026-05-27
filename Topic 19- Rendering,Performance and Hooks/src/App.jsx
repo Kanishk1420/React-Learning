f13413ee249ad8fd78532391cc7c057b9d930bec
Kanishk <Kanishkgupta2003@outlook.com>
2026-05-27 15:04:36 +0530

    UseRef hook

---
+++ Topic 19- Rendering,Performance and Hooks/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {UseRefs}  from './hooks/useRefs/index'
+const App = () => {
+  return (
+    <div>
+      <UseRefs/>
+    </div>
+  )
+}
+
+export default App
---
+++ Topic 19- Rendering,Performance and Hooks/src/hooks/useRefs/index.jsx
@@ -0,0 +1,21 @@
+import { useRef } from 'react'
+export const UseRefs = () => {
+    // const usernameRef = document.getElementById('username');
+    // const passwordRef = document.getElementById('password');
+    // This is not the correct way to access DOM elements in React. We should use useRef instead. 
+    const passwordRef = useRef('');
+    const usernameRef = useRef('');
+    
+const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Username:", usernameRef.current.value);
+        console.log("Password:", passwordRef.current.value);
+}
+    return (
+        <form onSubmit={handleSubmit} className='flex flex-col gap-5'>
+            <input type="text" placeholder='Enter your name' id='username' ref={usernameRef}/>
+            <input type="text" placeholder='Enter your password' id='password' ref= {passwordRef}/>
+            <button>Submit</button>
+        </form>
+    )
+}
